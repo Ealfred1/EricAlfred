9c6081d01feb415962b9e82ddf530847901bd675
Ealfred1 <alfrederic371@gmail.com>
2024-04-11 23:49:40 +0100

    commit2

--- src/components/Projects/SingleItem.jsx
+++ src/components/Projects/SingleItem.jsx
@@ -17,6 +17,7 @@ const SingleItem = ({ item }) => {
         hidden: { opacity: 0, X: 300, y: 300 },
         visible: { opacity: 1, X: 0, y: 0, transition: { duration: 1, staggerChildren: 0.1 } },
     };
+    console.log(item.img)
 
 	return (
 		<section className="py-36">
@@ -28,7 +29,7 @@ const SingleItem = ({ item }) => {
             		ref={ref}
 					>
 					<div className="flex-1 h-[15rem]">						
-						<img src={img} alt={item.title} className="w-full h-full object-cover rounded-xl" />
+						<img src={item.img} alt={item.title} className="w-full h-full object-cover rounded-xl" />
 					</div>
 					<div className="text-container flex-1 flex flex-col gap-[20px] z-[2]">
 						<h2 className="text-[2rem] font-bold">{ item.title }</h2>
